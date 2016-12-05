5cb706607eab6bd2e298b6efb15468163bd200c5
Artur Pinchukov <Artur Pinchukov>
2016-12-05 03:49:26 +0300

    add spring security

--- src/main/java/ru/pinch/controller/MainController.java
+++ src/main/java/ru/pinch/controller/MainController.java
@@ -1,5 +1,6 @@
 package ru.pinch.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
